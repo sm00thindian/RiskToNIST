9714ecd13e9b10e354e4a886afb0a46f919b3a62
Kilynn Weber <cheesy.email@gmail.com>
2025-06-08 19:45:40 -0500

    Update risk_calculation.py

--- src/risk_calculation.py
+++ src/risk_calculation.py
@@ -1,6 +1,6 @@
 from collections import defaultdict
 
-def calculate_control_risks(kev_cves, cve_to_techniques, technique_to_controls, satisfied_controls):
+def calculate_control_risks(kev_cves, cve_to_techniques, technique_to_controls):
     control_to_cves = defaultdict(set)
     for cve in kev_cves:
         if cve in cve_to_techniques:
@@ -10,8 +10,7 @@ def calculate_control_risks(kev_cves, cve_to_techniques, technique_to_controls, 
                     for control in technique_to_controls[tech]:
                         control_to_cves[control].add(cve)
     
-    control_to_risk = {control: len(cves) for control, cves in control_to_cves.items() if control in satisfied_controls}
-    covered_cves = set.union(*(control_to_cves[control] for control in satisfied_controls)) if satisfied_controls else set()
-    total_cves = set(kev_cves)
+    control_to_risk = {control: len(cves) for control, cves in control_to_cves.items()}
+    total_cves = len(kev_cves)
     
-    return control_to_risk, covered_cves, total_cves
+    return control_to_risk, total_cves
